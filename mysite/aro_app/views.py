fc58d030c13b4d525329a97b7ec5f4c6f9a17c97
saiful-aromax <saiful.aromax@gmail.com>
2022-06-03 06:10:03 +0600

    first commit

---
+++ mysite/aro_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . utility import layout
+
+# Create your views here.
+def home(request):
+ AroVar = {"alpha":"A", "bita": "B", "gamma": 'C'}
+ return layout(request, 'home.html')
+ 
+ # return layout(request, 'home.html', AroVar)
+  # return render(request, 'layout.html')
