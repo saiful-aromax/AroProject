d63305c1b730aade91fe6d619db9bbba0fc7ee4f
saiful-aromax <saiful.aromax@gmail.com>
2022-06-03 06:47:00 +0600

    latest layout

--- mysite/aro_app/views.py
+++ mysite/aro_app/views.py
@@ -5,7 +5,7 @@ from . utility import layout
 # Create your views here.
 def home(request):
  AroVar = {"alpha":"A", "bita": "B", "gamma": 'C'}
- return layout(request, 'home.html')
+ return layout(request, 'home.html', AroVar)
  
  # return layout(request, 'home.html', AroVar)
   # return render(request, 'layout.html')
